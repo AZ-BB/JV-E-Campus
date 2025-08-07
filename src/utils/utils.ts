67ec7c5b9893872388e129dba0a19bd5342bd8ec
anas12312 <anashesham000@gmail.com>
2025-08-07 10:33:38 +0300

    feat: added icons directory to the bucket

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,7 +3,9 @@ import { createSupabaseServerClient } from "./supabase-server"
 export const mapAvatarImageUrl = (imageUrl: string) => {
     return `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/${process.env.NEXT_PUBLIC_SUPABASE_STORAGE_NAME}/${process.env.NEXT_PUBLIC_STORAGE_AVATAR_DIRECTORY}/${imageUrl}`
 }
-
+export const mapIconUrl = (imageUrl: string) => {
+    return `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/${process.env.NEXT_PUBLIC_SUPABASE_STORAGE_NAME}/${process.env.NEXT_PUBLIC_STORAGE_ICONS_DIRECTORY}/${imageUrl}`
+}
 export const getCurrentUser = async () => {
     const supabase = await createSupabaseServerClient()
 
